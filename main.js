f1c358fcef12fa1337ce41159149d2c2b10ff4fe
Liendo00 <liendoelian00@gmail.com>
2021-02-22 14:58:38 -0300

    mapa

---
+++ main.js
@@ -0,0 +1,23 @@
+const WIDTH = 400;
+const HEIGH = 400;
+
+let target = {
+    x : getRandomNumber(WIDTH), 
+    y : getRandomNumber(HEIGH)
+};
+
+let $map = document.querySelector (".map");
+let $distance = document.querySelector(".distance");
+let clicks = 0;
+
+$map.addEventListener("click", function (e) {
+    clicks++;
+    let distance = getDistance(e, target);
+    let distanceHint = getDistanceHint(distance);
+    $distance.innerHTML = `<h1>${distanceHint}</h1>`;
+
+    if(distance < 20 ){
+        alert (`Has encontrado el tesoro en ${clicks} clicks !`);
+        location.reload();
+    }
+})
